0ee81640b7f5b50cb184a5d55f9c87fc65ae3ad1
walze <catufuzgu@gmail.com>
2020-02-03 06:23:58 -0300

    added droplet

--- src/components/WeatherCard/index.tsx
+++ src/components/WeatherCard/index.tsx
@@ -1,8 +1,7 @@
 
 import React, { FunctionComponent } from 'react'
 import { Weather } from '~types/weather'
-import { Card } from './style'
-import { map } from 'ramda'
+import { Card, Droplet } from './style'
 
 const WeatherCard: FunctionComponent<Weather> = (props) => {
   console.log(props, props.main)
@@ -13,20 +12,24 @@ const WeatherCard: FunctionComponent<Weather> = (props) => {
     <Card className="card mb-3">
 
       <div className="row no-gutters">
-        <div className="col-md-4">
-          <img src={`http://openweathermap.org/img/wn/${climate.icon}@2x.png`} alt={`Photo of ${climate.main}`} className="card-img" />
+        <div className="col-md-4 d-flex align-items-center">
+          <img src={`http://openweathermap.org/img/wn/${climate.icon}@2x.png`} alt={`Photo of ${climate.main}`} />
+
+          <div>
+            <h5 className="card-title">{climate.main}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">{climate.description}</h6>
+          </div>
+
         </div>
         <div className="col-md-8">
           <div className="card-body">
-            <h5 className="card-title">{climate.main}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">{climate.description}</h6>
 
             <p>Temperature - {main.temp}</p>
             <p>Feels like - {main.feels_like}</p>
             <p>Minimum - {main.temp_min}</p>
             <p>Maximum - {main.temp_max}</p>
             <p>Pressure - {main.pressure}</p>
-            <p>Humidity - {main.humidity}</p>
+            <div><Droplet /> {main.humidity}%</div>
 
             <p >
               {name}, {sys.country}
--- src/components/WeatherCard/style.ts
+++ src/components/WeatherCard/style.ts
@@ -1,8 +1,24 @@
 
 import styled from 'styled-components'
 
+const dropletSize = 15
+
+export const Droplet = styled.div`
+    display: inline-block;
+
+    width: ${dropletSize}px;
+    height: ${dropletSize}px;
+
+    border-top-left-radius: 0;
+    border-top-right-radius: 75px;
+    border-bottom-right-radius: 75px;
+    border-bottom-left-radius: 75px;
+
+    border: 1px solid rgba(0, 0, 0, .1);
+    transform: rotate(45deg);
+    background: lightskyblue;
+`
 export const Card = styled.div`
   min-width: 320px;
   max-width: 640px;
-
 `
